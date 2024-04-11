c1ae1c0cdc42e11d8f9edee338b84c7374c1acde
GauravDhal <157561131+GauravDhal@users.noreply.github.com>
2024-04-11 13:37:17 +0100

    Add files via upload

--- MiniProject.py
+++ MiniProject.py
@@ -21,3 +21,28 @@ while(sum<=1):
     r=r+1
     
 print(r-1)    
+
+#randomly filling
+n=6
+glasses = np.zeros(n)
+win=False #Ali wins
+while(win!=True):
+    l = 0.5 #liquid remaining
+    attempt = np.zeros(n)
+    for i in range(0,n-1):
+        attempt[i] = np.random.uniform(0,l)
+        l = l-attempt[i]
+     
+    attempt[n-1]=l
+    glasses = glasses + attempt
+    print("Ali's turn",glasses)
+
+    #check if overflowing
+    if(np.max(glasses) >1):
+        win=True
+    
+    #emptying glasses
+    glasses[np.argmax(glasses)]=0
+    glasses[ (np.argmax(glasses)-1) % n ]=0
+
+    print("Beth's turn",glasses)
